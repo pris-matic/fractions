5114ab9cc7e81b3b9a02dc3447e5a450fd2d1b6b
ajkiopal <jackyyao20104@gmail.com>
2025-02-01 18:00:57 +0800

    resolved some errors with string conversion

--- Fraction.py
+++ Fraction.py
@@ -5,38 +5,46 @@ class Fraction(object):
         # if numerator is a string
         if isinstance(numerator,str):
             fraction_values = numerator.split("/")
-            if len(fraction_values) > 2:
+            if len(fraction_values) < 2:
                 self._numerator = 0
                 self._denominator = denominator
             else:
-                self._numerator = fraction_values[0]
-                self._denominator = fraction_values[1]
+                self._numerator = int(fraction_values[0])
+                self._denominator = int(fraction_values[1])
 
         else:
             self._numerator = numerator
             self._denominator = denominator
 
-        # if the number provided is a float / double
-        if numerator != int(numerator):
-            self._numerator = 0
+            # if the number provided is a float / double
+            if numerator != int(numerator):
+                self._numerator = 0
 
         if denominator == 0:
             raise ZeroDivisionError("Denominator cannot be zero.")
 
-        pass
-
     def gcd(a, b):
-        #TODO
-        pass
+        
+        if a == 0 or b == 0:
+            return 0
+
+        while b != 0:
+            (a, b) = (b, a % b)
+        return a
 
     def get_numerator(self):
-        #TODO
-        pass
+        greatest_divisor = Fraction.gcd(self._numerator,self._denominator)
+        if greatest_divisor == 0:
+            return "0"
+        return str(int(self._numerator / greatest_divisor))
 
     def get_denominator(self):
-        #TODO
-        pass
+        greatest_divisor = Fraction.gcd(self._numerator,self._denominator)
+        if greatest_divisor == 0:
+            return "0"
+        return str(int(self._denominator / greatest_divisor))
 
     def get_fraction(self):
-        #TODO
-        pass
+        if self._denominator == 1 or self._numerator == 0:
+            return self.get_numerator()
+        return self.get_numerator() + "/" + self.get_denominator()
